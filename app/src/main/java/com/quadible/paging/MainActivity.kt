7127a1cf08fb48573a2a8260f9a2a416b9e75215
Vaios <vaios.tsitsonis@theblueground.com>
2021-10-30 21:11:20 +0300

    Start lazy loading data in MainActivity

--- app/src/main/java/com/quadible/paging/MainActivity.kt
+++ app/src/main/java/com/quadible/paging/MainActivity.kt
@@ -3,21 +3,38 @@ package com.quadible.paging
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.quadible.paging.ui.theme.AssertPageTheme
+import kotlinx.coroutines.flow.Flow
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels { MainViewModelFactory() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AssertPageTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    DummyDataListing(dummyDataPages = viewModel.getDummyData())
                 }
             }
         }
@@ -25,14 +42,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DummyDataListing(
+    dummyDataPages: Flow<PagingData<DummyData>>
+) {
+    val lazyDummyDataItems = dummyDataPages.collectAsLazyPagingItems()
+    LazyColumn(
+        content = {
+            items(lazyDummyDataItems.itemCount) { index ->
+                lazyDummyDataItems[index]?.let { DummyDataItem(dummyData = it) }
+            }
+        }
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AssertPageTheme {
-        Greeting("Android")
+fun DummyDataItem(modifier: Modifier = Modifier, dummyData: DummyData) {
+    Box(
+        modifier = modifier
+            .height(56.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { /* Ignoring onClick */ })
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterStart).padding(16.dp),
+            text = dummyData.someValue,
+            color = Color.Black
+        )
+        Divider()
     }
 }
