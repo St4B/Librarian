8a782af68cb77a8329ad004ff8b19072e8882cd2
Vaios <vaios.tsitsonis@theblueground.com>
2021-10-30 20:26:18 +0300

    Add 'DummyPagingSource'

---
+++ app/src/main/java/com/quadible/paging/DummyData.kt
@@ -0,0 +1,25 @@
+package com.quadible.paging
+
+data class DummyData(
+    val id: Int,
+    val someValue: String
+)
+
+val pagedData = listOf(
+    DummyData(id = 1, someValue = "first value"),
+    DummyData(id = 2, someValue = "second value"),
+    DummyData(id = 3, someValue = "third value"),
+    DummyData(id = 4, someValue = "fourth value"),
+    DummyData(id = 5, someValue = "fifth value"),
+    DummyData(id = 6, someValue = "sixth value"),
+    DummyData(id = 7, someValue = "seventh value"),
+    DummyData(id = 8, someValue = "eighth value"),
+    DummyData(id = 9, someValue = "ninth value"),
+    DummyData(id = 10, someValue = "tenth value"),
+    DummyData(id = 11, someValue = "eleventh value"),
+    DummyData(id = 12, someValue = "twelfth value"),
+    DummyData(id = 13, someValue = "thirteenth value"),
+    DummyData(id = 14, someValue = "fourteenth value"),
+    DummyData(id = 15, someValue = "fifteenth value"),
+    DummyData(id = 16, someValue = "sixteenth value")
+)
---
+++ app/src/main/java/com/quadible/paging/DummyPagingSource.kt
@@ -0,0 +1,38 @@
+package com.quadible.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+
+class DummyPagingSource(private val pageSize: Int) : PagingSource<Int, DummyData>() {
+
+    // prevKey == null -> anchorPage is the first page.
+    // nextKey == null -> anchorPage is the last page.
+    // both prevKey and nextKey null -> anchorPage is the initial page.
+    override fun getRefreshKey(state: PagingState<Int, DummyData>): Int? =
+        state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, DummyData> = try {
+        val pageNumber = when (params) {
+            is LoadParams.Refresh -> 0
+            else -> params.key ?: 0
+        }
+        val page = pagedData.chunked(pageSize).getOrElse(pageNumber) { emptyList() }
+
+        val nextPageNumber = if (page.isEmpty()) {
+            null
+        } else {
+            pageNumber + 1
+        }
+
+        LoadResult.Page(
+            data = page,
+            prevKey = null, // Only paging forward.
+            nextKey = nextPageNumber
+        )
+    } catch (e: Exception) {
+        LoadResult.Error(throwable = e)
+    }
+}
